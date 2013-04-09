92a507b5c3e8ed7ad77ab7ee94240bdd9250677d
Ron Lisle <ron@lisles.net>
2013-04-09 10:34:41 -0500

    Add basic Kiwi test case file

---
+++ Custom Unit Tests/Kiwi unit test case file.xctemplate/___FILEBASENAME___Tests.m
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Kiwi test cases for ___FILEBASENAMEASIDENTIFIER___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAMEASIDENTIFIER___.h"
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Tests)
+
+context(@"___FILEBASENAMEASIDENTIFIER___", ^{
+
+    __block ___FILEBASENAMEASIDENTIFIER___ *testObj;
+
+    beforeEach( ^{
+        testobj = [[___FILEBASENAMEASIDENTIFIER___ alloc] init];
+    });
+
+    describe(@"when something happens", ^{
+        
+        it(@"does something", ^{
+            
+            //...
+            
+        });
+        
+    });
+    
+});
+
+SPEC_END
